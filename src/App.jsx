f6b90cd5c33a5708e020d2bb99938dfe61d1cd2d
Breno Norberto <brenonorberto@gmail.com>
2021-03-22 13:01:52 -0300

    comunicação direta

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,18 @@ import Card from './layout/Card'
 import Repeticao from './components/basicos/Repeticao'
 import Condicional from './components/basicos/Condicional'
 import CondicionalComIf from './components/basicos/CondicionalComIf'
+import Pai from './components/comunicacao/direta/Pai';
 
 export default (props) => (
     <div className="App">
-        <Card titulo='#01 - Primeiro Componente'>
+        <Card titulo='#01 - Primeiro Componente' color='#FA6900'>
         <Primeiro />
         </Card>
-        <Card titulo='#02 - Componente Com Parâmetro '>
+        <Card titulo='#02 - Componente Com Parâmetro' color='#E94C6F'>
             <ComParametro titulo='Esse é o Titulo' subtitulo='Esse é o Subtitulo' />
             <ComParametro titulo='Opa' subtitulo='Epa' />
         </Card>
-        <Card titulo='#03 - Componente Com Filhos '>
+        <Card titulo='#03 - Componente Com Filhos' color='#008BBA'>
             <ComFilhos>
             <ul>
                 <li>Ana</li>
@@ -27,14 +28,17 @@ export default (props) => (
             </ul>
             </ComFilhos>    
         </Card>
-        <Card titulo='#04 - Repetição'>
+        <Card titulo='#04 - Repetição' color='#D96459'>
         <Repeticao />
         </Card>
-        <Card titulo='#05 - Condicional v1'>
+        <Card titulo='#05 - Condicional v1' color='#FF85CB'>
         <Condicional numero={3}/>
         </Card>
-        <Card titulo='#06 - Condicional v2'>
+        <Card titulo='#06 - Condicional v2' color='#92B0CA'>
         <CondicionalComIf numero={10}/>
+        </Card>
+        <Card titulo='#07 - Comunicação Direta' color='#4298B5'>
+        <Pai sobrenome='Freitas'/>
         </Card>
   </div>
 )
--- src/components/basicos/ComParametro.jsx
+++ src/components/basicos/ComParametro.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default props => // props são os parâmetros
+export default props => // props são os parâmetros, e são somente leitura, não pode mudar
     <>
         <h3>{props.titulo}</h3>
         <p>{props.subtitulo}</p>
